da92f24e92f6c12ac7cd149e12409ab619a8392d
rplinzner <37122411+rplinzner@users.noreply.github.com>
2019-03-09 18:23:11 +0100

    [WIP] Function generator

---
+++ Signal_generators/Generator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Signal_generators
+{
+    public class Generator
+    {
+        public double Amplitude { get; set; }
+        public double Period { get; set; }
+        public double Frequency { get; set; }
+        public double StartTime { get; set; }
+        public double Duration { get; set; }
+        public double TimeS { get; set; }
+
+        private Random rand = new Random();
+
+        public double SteadyNoise()
+        {
+            return rand.NextDouble() * 2*Amplitude - Amplitude;
+        }
+        private double Sinus(int time)
+        {
+            return Math.Sin(((2 * Math.PI) / Period) * (time - StartTime));
+        }
+
+        public double Sinusoidal(int time)
+        {
+            return Amplitude * Sinus(time);
+        }
+
+        public double Sinusoidal1P(int time)
+        {
+            return 0.5 * Amplitude * (Sinus(time) + Math.Abs(Sinus(time)));
+        }
+        public double Sinusoidal2P(int time)
+        {
+            return  Amplitude * Math.Abs(Sinus(time));
+        }
+
+        public double Rectangural(int time)
+        {
+
+        }
+
+        public double UnitJump(int time)
+        {
+            if (time > TimeS)
+            {
+                return Amplitude;
+            }
+
+            if (time == TimeS)
+            {
+                return 0.5 * Amplitude;
+            }
+
+            if (time < TimeS)
+            {
+                return 0;
+            }
+
+            return 0;
+        }
+       
+    }
+}
