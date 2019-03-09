a0adb4e94c2e90619b61eefb4eb92d425a1901c1
rplinzner <37122411+rplinzner@users.noreply.github.com>
2019-03-09 20:21:16 +0100

    Update Generator.cs

--- Signal_generators/Generator.cs
+++ Signal_generators/Generator.cs
@@ -9,17 +9,22 @@ namespace Signal_generators
         public double Frequency { get; set; }
         public double StartTime { get; set; }
         public double Duration { get; set; }
-        public double TimeS { get; set; }
+        public double STime { get; set; } //For Unit Jump
+        public double FillFactor { get; set; }  //For Rectangular and Triangular
+        private int KFactor { get; set; }
 
         private Random rand = new Random();
 
+
+        //TODO: All Discrete Signals + Gaussian noise
+        //https://stackoverflow.com/questions/218060/random-gaussian-variables - Gaussian noise
         public double SteadyNoise()
         {
-            return rand.NextDouble() * 2*Amplitude - Amplitude;
+            return rand.NextDouble() * 2 * Amplitude - Amplitude;
         }
         private double Sinus(int time)
         {
-            return Math.Sin(((2 * Math.PI) / Period) * (time - StartTime));
+            return Math.Sin(2 * Math.PI / Period * (time - StartTime));
         }
 
         public double Sinusoidal(int time)
@@ -33,33 +38,54 @@ namespace Signal_generators
         }
         public double Sinusoidal2P(int time)
         {
-            return  Amplitude * Math.Abs(Sinus(time));
+            return Amplitude * Math.Abs(Sinus(time));
+        }
+
+        private bool Rect(int time)
+        {
+            KFactor = (int)(time / Period);
+            if (time >= KFactor * Period + StartTime && time < FillFactor * Period + KFactor * Period + StartTime)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public double Rectangural(int time)
         {
+            if (Rect(time)) return Amplitude;
+            return 0;
+        }
+        public double RectanguralSimetrical(int time)
+        {
+            if (Rect(time)) return Amplitude;
+            return -Amplitude;
+        }
 
+        public double Triangular(int time)
+        {
+            if (Rect(time))
+            {
+                return Amplitude / (FillFactor * Period) * (time - KFactor * Period - StartTime);
+            }
+            return -Amplitude / (Period * (1 - FillFactor)) * (time - KFactor * Period - StartTime) +
+                   (Amplitude / (1 - FillFactor));
         }
 
         public double UnitJump(int time)
         {
-            if (time > TimeS)
+            if (time > STime)
             {
                 return Amplitude;
             }
 
-            if (time == TimeS)
+            if (time == STime)
             {
                 return 0.5 * Amplitude;
             }
-
-            if (time < TimeS)
-            {
-                return 0;
-            }
-
             return 0;
         }
-       
+
     }
 }
